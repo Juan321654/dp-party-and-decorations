2e32a67c3c4647aed253647de52a34481653159d
juan camacho <juanc@tiv.solutions>
2021-08-10 18:50:55 -0400

    added api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './screens/header/Header'
 import Navbar from './screens/navbar/Navbar';
 import MidHeader from './screens/midHeader/MidHeader';
+import axios from 'axios'
+
+let key = process.env.REACT_APP_key
+let base = process.env.REACT_APP_base
 
 function App() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    let url = `https://api.airtable.com/v0/${base}/content?api_key=${key}`
+    const getData = async() => {
+      try {
+        const res = await axios.get(url)
+        setData(res)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+
   return (
     <div className="App">
       <Header />
