ef4039bdfddcf2d8a6cfcf78a0b92104723867a5
juan camacho <juanc@tiv.solutions>
2021-08-11 19:26:51 -0400

    made better picture displays

--- src/screens/examples/Examples.jsx
+++ src/screens/examples/Examples.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import './examples.css'
+import React from "react";
+import "./examples.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 
 function Examples({ data }) {
-
-  // console.log(data.records)
-  function createCards(){
-    return data.records.map((e, idx) => {
-      console.log(e)
-      return (
-        <div key={idx} className="card__container">
-          <img src={e.fields.attachment[0].url} alt="party" />
-          <p style={{fontWeight: 'bold'}}>{e.fields.attachment_title}</p>
-          <p>{e.fields.attachment_description}</p>
-        </div>
-      )
-    })
+  function createCards() {
+    return data?.records.map((e, idx) => {
+      console.log(e);
+      if (e.fields.attachment)
+        return (
+          <Card key={idx} style={{ width: "20rem", margin: "1.25rem" }} className="card-hover">
+            <a href={e.fields.attachment[0].url} target="_blank">
+              <Card.Img
+                src={e.fields.attachment[0].thumbnails.large.url}
+                alt="party"
+                style={{
+                  height: "20rem",
+                  objectFit: "scale-down",
+                  backgroundColor: "black",
+                  cursor: "zoom-in",
+                }}
+              />
+            </a>
+            <Card.Body>
+              <Card.Title>{e?.fields.attachment_title}</Card.Title>
+              <Card.Text>
+                {e?.fields.attachment_description}
+                <br />
+                {e?.fields.price}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        );
+    });
   }
-  return (
-    <div className="example__container">
-      {createCards()}
-    </div>
-  )
+
+  return <div className="example__container">{createCards()}</div>;
 }
 
-export default Examples
+export default Examples;
